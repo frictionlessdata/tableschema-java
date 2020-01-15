827d7e194d702fa8ffe027bb341f64d43d2c057c
Johannes Jander <139699+iSnow@users.noreply.github.com>
2020-01-15 11:14:35 +0100

    Fix and Test for JSON data with missing properties (== null values), see https://github.com/frictionlessdata/specs/issues/656 (#46)
    Co-authored-by: JohannesJander <56732599+JohannesJander@users.noreply.github.com>

--- src/main/java/io/frictionlessdata/tableschema/datasourceformat/JsonArrayDataSourceFormat.java
+++ src/main/java/io/frictionlessdata/tableschema/datasourceformat/JsonArrayDataSourceFormat.java
@@ -1,8 +1,11 @@
 package io.frictionlessdata.tableschema.datasourceformat;
 
+import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.json.CDL;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -37,11 +40,57 @@ public class JsonArrayDataSourceFormat extends AbstractDataSourceFormat {
         this.dataSource = new JSONArray(content);
     }
 
+    /**
+     * Write as CSV file, the `format` parameter decides on the CSV options. If it is
+     * null, then the file will be written as RFC 4180 compliant CSV
+     * @param out the Writer to write to
+     * @param format the CSV format to use
+     * @param sortedHeaders the header row names in the order in which data should be
+     *                      exported
+     */
+    @Override
+    public void writeCsv(Writer out, CSVFormat format, String[] sortedHeaders) {
+        if (null == sortedHeaders) {
+            writeCsv(out, format);
+            return;
+        }
+        try {
+            CSVFormat locFormat = (null != format)
+                    ? format
+                    : DataSourceFormat.getDefaultCsvFormat();
+
+            locFormat = locFormat.withHeader(sortedHeaders);
+            CSVPrinter csvPrinter = new CSVPrinter(out, locFormat);
+            JSONArray data = (JSONArray)this.dataSource;
+            for (Object record : data) {
+                JSONObject obj = (JSONObject) record;
+                writeData(obj, sortedHeaders, csvPrinter);
+            }
+            csvPrinter.close();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static void writeData(JSONObject data, String[] sortedHeaders, CSVPrinter csvPrinter) {
+        try {
+            int recordLength = sortedHeaders.length;
+            String[] sortedRec = new String[recordLength];
+            for (int i = 0; i < recordLength; i++) {
+                String key = sortedHeaders[i];
+                if (data.has(key))
+                    sortedRec[i] = data.get(key).toString();
+            }
+            csvPrinter.printRecord(sortedRec);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
     @Override
     public void write(File outputFile) throws Exception {
         try (Writer out = new BufferedWriter(new FileWriter(outputFile))) {
-            out.write(((JSONArray)dataSource).toString());
+            out.write(dataSource.toString());
         } catch (Exception e) {
             throw e;
         }
